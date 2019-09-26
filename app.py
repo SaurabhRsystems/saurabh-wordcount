dcf1478b30ac1c6d1c471e95e4ead284b183a833
Saurabh Goel <Saurabh.Goel@rsystems.com>
2019-09-26 19:39:18 +0530

    initial_commit_01

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
+print(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -11,4 +14,5 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+    print(__file__)
     app.run()
